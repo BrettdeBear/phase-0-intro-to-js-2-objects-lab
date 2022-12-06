2b978c11179bce115e7f7b0368c9b1c710a521a4
Brett de Bear <bkdebear@gmail.com>
2022-12-06 12:20:17 -0800

    commit tree c76c28803cb87f0fc0eb4844fce3a0d5ef4b1e3f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Brett",
+    streetAddress: "addressHere",
+};
+employee.name = "Sam";
+function updateEmployeeWithKeyAndValue(employee, streetAddress, addressHere) {
+    return {
+        ...employee,
+        [streetAddress]: "11 Broadway",
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, addressHere) {
+    employee[streetAddress] = "12 Broadway";
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+};
