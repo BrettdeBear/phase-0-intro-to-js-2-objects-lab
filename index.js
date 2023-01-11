8d6566b1c1babcf2b0f0139e9f2d9d98ae0ece96
Brett de Bear <bkdebear@gmail.com>
2023-01-11 10:28:46 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Kula",
+    streetAddress: "63 Abrams Ct",
+};
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+    return {
+        ...employee,
+        [streetAddress]: "11 Broadway",
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return {
+        newEmployee,
+    }
+    }
+
+    function destructivelyDeleteFromEmployeeByKey(employee, name) {
+        delete employee.name;
+        return employee;
+    }
